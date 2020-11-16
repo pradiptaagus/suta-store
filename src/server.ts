413a3a80bc0a3d70e2d697c83cdec33ff2a02df2
pradiptaagus <igedeaguspradipta@gmail.com>
2020-11-16 17:30:00 +0800

    import product detail controller

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { createConnection } from "typeorm";
 import { ProductController } from "./app/product/product.controller";
 import { VersionController } from "./app/version/version.controller";
 import { TransactionController } from "./app/transaction/transaction.controller";
+import { ProductDetailController } from "./app/product-detail/product-detail.controller";
 
 dotenv.config();
 const defaultPort: number = 1200;
@@ -13,7 +14,8 @@ createConnection().then(() => {
     const app = new App([
         new ProductController(),
         new VersionController(),
-        new TransactionController()
+        new TransactionController(),
+        new ProductDetailController()
     ], port);
     app.listen();
 }).catch(error => {
