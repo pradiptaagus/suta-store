1bfd98476d6938a8e1e9cf59de8525ed201b34eb
pradiptaagus <igedeaguspradipta@gmail.com>
2020-11-20 11:51:04 +0800

    import dashboard controller in server.ts

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import { ProductController } from "./app/product/product.controller";
 import { VersionController } from "./app/version/version.controller";
 import { TransactionController } from "./app/transaction/transaction.controller";
 import { ProductDetailController } from "./app/product-detail/product-detail.controller";
+import { DashboardController } from "./app/dashboard/dashboard.controller";
 
 dotenv.config();
 const defaultPort: number = 1200;
@@ -15,7 +16,8 @@ createConnection().then(() => {
         new ProductController(),
         new VersionController(),
         new TransactionController(),
-        new ProductDetailController()
+        new ProductDetailController(),
+        new DashboardController()
     ], port);
     app.listen();
 }).catch(error => {
