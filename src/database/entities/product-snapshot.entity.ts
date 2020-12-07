66204db9402141e5e1cda69ff49f39d96828cbac
pradiptaagus <igedeaguspradipta@gmail.com>
2020-12-07 19:25:59 +0800

    add product-snapshot table relation to transaction

--- src/database/entities/product-snapshot.entity.ts
+++ src/database/entities/product-snapshot.entity.ts
@@ -1,14 +1,17 @@
-import { Column, CreateDateColumn, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn, Timestamp, UpdateDateColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, Timestamp, UpdateDateColumn } from "typeorm";
 import { ProductDetail } from "./product-detail.entity";
-import { TransactionDetail } from "./transaction-detail.entity";
+import { Transaction } from "./transaction.entity";
 
 @Entity({name: "product_snapshots"})
 export class ProductSnapshot {
     @PrimaryGeneratedColumn("uuid")
     id!: string;
 
-    @ManyToOne(type => ProductDetail, productDetail => productDetail.productSnapshot)
+    @ManyToOne(() => ProductDetail, productDetail => productDetail.productSnapshot)
     productVariant!: ProductDetail;
+
+    @ManyToOne(() => Transaction, transaction => transaction.productSnapshot)
+    transaction!: Transaction;
 
     @Column({type: "varchar", length: 50})
     code!: string;
@@ -39,7 +42,4 @@ export class ProductSnapshot {
 
     @UpdateDateColumn()
     updatedAt!: Timestamp;
-
-    @OneToMany(type => TransactionDetail, transactionDetail => transactionDetail.productSnapshot)
-    transactionDetail!: TransactionDetail;
 }
